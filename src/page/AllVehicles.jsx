eafbb7a4a3cf26cf3233231533968d5f4bf114d6
md-rafiul-islam <rafiulislam2001@gmail.com>
2026-01-19 15:45:58 +0600

    all vehicles page added without searchbox and filtering

--- src/page/AllVehicles.jsx
+++ src/page/AllVehicles.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { useLoaderData } from "react-router";
+import Card from "../components/Card";
 
 const AllVehicles = () => {
   const vehiclesData = useLoaderData();
   console.log(vehiclesData);
   return (
     <div>
-      <p>we will see all vehicles here</p>
+      <section className="max-w-7xl mx-auto px-4 py-10 text-center">
+        <h1 className="text-3xl md:text-4xl font-bold text-gray-900">
+          All Vehicles
+        </h1>
+        <p className="mt-2 text-gray-600">
+          Choose from a wide range of vehicles available for rent
+        </p>
+      </section>
+
+      <section className="max-w-11/12 mx-auto gap-3 grid sm:grid-cols-2 lg:grid-cols-3">
+        {vehiclesData.map((vehicle) => (
+          <Card key={vehicle._id} vehicle={vehicle}></Card>
+        ))}
+      </section>
     </div>
   );
 };
